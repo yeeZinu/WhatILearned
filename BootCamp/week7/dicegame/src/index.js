7012fd2d0c549af37533b963f7274e80f2163dd7
yeeZinu <doglife222@gmail.com>
2024-02-04 12:30:23 +0900

    주사위 게임 초기세팅

---
+++ BootCamp/week7/dicegame/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>주 사위 게임</h1>
+  </React.StrictMode>
+);
