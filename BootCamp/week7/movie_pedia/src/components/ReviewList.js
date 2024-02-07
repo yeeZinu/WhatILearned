0ed0849e9885f56e3f21b2cb4fea919f5d17c553
yeeZinu <doglife222@gmail.com>
2024-02-07 23:16:23 +0900

    map을 통한 데이터 렌더링

--- BootCamp/week7/movie_pedia/src/components/ReviewList.js
+++ BootCamp/week7/movie_pedia/src/components/ReviewList.js
@@ -1,8 +1,32 @@
+import './ReviewList.css';
+
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
+function ReviewItem({ item }) {
+  return (
+    <div className='ReviewListItem'>
+      <img className='ReviewListItem-img' src={item.imgUrl} alt={item.title} />
+      <div>
+        <h1>{item.title}</h1>
+        <p>{item.rating}</p>
+        <p>{formatDate(item.createdAt)}</p>
+        <p>{item.content}</p>
+      </div>
+    </div>
+  );
+}
+
 function Reviewlist({ items }) {
   console.log(items);
   return (
-    <div>
-    </div>
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}><ReviewItem item={item} /></li>
+      ))}
+    </ul>
   );
 
 }
