19851a0d2f41516d6f8caa76d18ecaba59d0d787
yeeZinu <doglife222@gmail.com>
2024-02-08 14:36:58 +0900

    최신순, 별점순 정렬

--- BootCamp/week7/movie_pedia/src/components/App.js
+++ BootCamp/week7/movie_pedia/src/components/App.js
@@ -1,9 +1,28 @@
 import Reviewlist from "./ReviewList";
 import items from '../mock.json';
+import { useState } from "react";
 
 function App() {
+  const [order, setOrder] = useState('createdAt');
+
+  const handleNewsSort = () => {
+    setOrder('createdAt');
+  };
+
+  const handleRateSort = () => {
+    setOrder('rating');
+  };
+
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
   return (
-    <Reviewlist items={items} />
+    <div>
+      <div>
+        <button onClick={handleNewsSort}>최신순</button>
+        <button onClick={handleRateSort}>베스트순</button>
+      </div>
+      <Reviewlist items={sortedItems} />
+    </div>
   );
 }
 
