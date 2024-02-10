13dfe73edd66e6fa45c0e5714650958e609bc4ed
yeeZinu <doglife222@gmail.com>
2024-02-10 23:31:08 +0900

    더보기 버튼 조건부 렌더링

--- BootCamp/week7/movie_pedia/src/components/App.js
+++ BootCamp/week7/movie_pedia/src/components/App.js
@@ -47,7 +47,7 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      <button disabled={!hasNext} onClick={handleLoadMore}>더보기</button>
+      {hasNext && <button onClick={handleLoadMore}>더보기</button>}
     </div>
   );
 }
