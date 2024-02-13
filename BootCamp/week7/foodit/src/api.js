13f92d6af2d53e9c5e66ea31f519bb96828fd8d7
yeeZinu <doglife222@gmail.com>
2024-02-13 09:37:56 +0900

    검색기능과 api에러처리

--- BootCamp/week7/foodit/src/api.js
+++ BootCamp/week7/foodit/src/api.js
@@ -1,6 +1,9 @@
-export async function getFoods(order = 'createdAt') {
-  const qurey = `order=${order}`;
+export async function getFoods({ order = 'createdAt', cursor = '', limit = 10, search = ''}) {
+  const qurey = `order=${order}&cursor=${cursor}&limit=${limit}&search=${search}`;
   const response = await fetch(`https://learn.codeit.kr/6665/foods?${qurey}`);
+  if(!response.ok) {
+    throw new Error('요청이 실패했습니다.');
+  }
   const body = await response.json();
   return body;
 }
--- BootCamp/week7/foodit/src/components/App.js
+++ BootCamp/week7/foodit/src/components/App.js
@@ -1,40 +1,85 @@
-import FoodList from './FoodList';
 import { useEffect, useState } from 'react';
 import { getFoods } from '../api';
+import FoodList from './FoodList';
 
 function App() {
-  const [order, setSorted] = useState('createdAt');
+  const [order, setOrder] = useState('createdAt');
+  const [cursor, setCursor] = useState(null);
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
+  const [search, setSearch] = useState('');
 
-  const handleNewsSort = () => {
-    setSorted('createdAt');
-  };
+  const handleNewestClick = () => setOrder('createdAt');
 
-  const handleCalorieSort = () => {
-    setSorted('calorie');
-  };
+  const handleCalorieClick = () => setOrder('calorie');
 
   const handleDelete = (id) => {
-    const nextItems = items.filter((items) => items.id !== id);
+    const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
 
-  const handelFetchFoods = async (orderQuery) => {
-    const { foods } = await getFoods(orderQuery);
-    setItems(foods);
+  const handleLoad = async (options) => {
+    let result;
+    try {
+      setLoadingError(null);
+      setIsLoading(true);
+      result = await getFoods(options);
+    } catch (error) {
+      setLoadingError(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+    const {
+      foods,
+      paging: { nextCursor },
+    } = result;
+    if (!options.cursor) {
+      setItems(foods);
+    } else {
+      setItems((prevItems) => [...prevItems, ...foods]);
+    }
+    setCursor(nextCursor);
   };
 
-  useEffect(() => {
-    handelFetchFoods(order);
-  }, [order]);
+  const handleLoadMore = () => {
+    handleLoad({
+      order,
+      cursor,
+      search,
+    });
+  };
 
-  const sortItems = items.sort((a, b) => b[order] - a[order]);
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    setSearch(e.target['search'].value);
+  };
+
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  useEffect(() => {
+    handleLoad({
+      order,
+      search,
+    });
+  }, [order, search]);
 
   return (
     <div>
-      <button onClick={handleNewsSort}>최신순</button>
-      <button onClick={handleCalorieSort}>칼로리순</button>
-      <FoodList items={sortItems} onDelete={handleDelete} />
+      <button onClick={handleNewestClick}>최신순</button>
+      <button onClick={handleCalorieClick}>칼로리순</button>
+      <form onSubmit={handleSearchSubmit}>
+        <input name="search" />
+        <button type="submit">검색</button>
+      </form>
+      <FoodList items={sortedItems} onDelete={handleDelete} />
+      {cursor && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더보기
+        </button>
+      )}
+      {loadingError?.message && <p>{loadingError.message}</p>}
     </div>
   );
 }
