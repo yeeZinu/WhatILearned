9608d35d68f1ef7995447fa0d58005da4ad03d1a
yeeZinu <doglife222@gmail.com>
2024-02-19 16:36:55 +0900

    삭제 api 연동

--- BootCamp/week7/foodit/src/components/App.js
+++ BootCamp/week7/foodit/src/components/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { createFood, getFoods, updateFood } from '../api';
+import { createFood, deleteFood, getFoods, updateFood } from '../api';
 import FoodList from './FoodList';
 import FoodForm from './FoodForm';
 
@@ -15,9 +15,11 @@ function App() {
 
   const handleCalorieClick = () => setOrder('calorie');
 
-  const handleDelete = (id) => {
-    const nextItems = items.filter((item) => item.id !== id);
-    setItems(nextItems);
+  const handleDelete = async (id) => {
+    const result = await deleteFood(id);
+    if (!result) return;
+
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
   };
 
   const handleLoad = async (options) => {
