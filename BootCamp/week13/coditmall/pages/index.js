d4957bae1d9fd427aca2cd71390b21cb30f02f07
yeeZinu <doglife222@gmail.com>
2024-03-27 22:25:07 +0900

    정적생성

--- BootCamp/week13/coditmall/pages/index.js
+++ BootCamp/week13/coditmall/pages/index.js
@@ -5,18 +5,18 @@ import axios from '@/lib/axios';
 import styles from '@/styles/Home.module.css';
 import Head from 'next/head';
 
-export default function Home() {
-  const [products, setProducts] = useState([]);
+export async function getStaticProps() {
+  const res = await axios.get('/products');
+  const products = res.data.results;
 
-  async function getProducts() {
-    const res = await axios.get('/products');
-    const nextProducts = res.data.results;
-    setProducts(nextProducts);
+  return {
+    props: {
+      products
+    }
   }
+}
 
-  useEffect(() => {
-    getProducts();
-  }, []);
+export default function Home({ products }) {
 
   return (
     <>
--- BootCamp/week13/coditmall/pages/items/[id].js
+++ BootCamp/week13/coditmall/pages/items/[id].js
@@ -5,18 +5,44 @@ import styles from '@/styles/Product.module.css';
 import SizeReviewList from '@/components/SizeReviewList';
 import StarRating from '@/components/StarRating';
 import Image from 'next/image';
+import Spinner from '@/components/Spinner';
 
-export default function Product() {
-  const [product, setProduct] = useState();
+export async function getStaticPaths() {
+  const res = await axios.get('/products');
+  const products = res.data.results;
+  const paths = products.map((product) => ({
+    params: { id: String(product.id) },
+  }));
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export async function getStaticProps(context) {
+  const productId = context.params['id'];
+  let product;
+  try {
+    const res = await axios.get(`/products/${productId}`);
+    product = res.data;
+  } catch {
+    return {
+      notFound: true,
+    };
+  }
+
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
+
+export default function Product({ product }) {
   const [sizeReviews, setSizeReviews] = useState([]);
   const router = useRouter();
   const { id } = router.query;
-
-  async function getProduct(targetId) {
-    const res = await axios.get(`/products/${targetId}`);
-    const nextProduct = res.data;
-    setProduct(nextProduct);
-  }
 
   async function getSizeReviews(targetId) {
     const res = await axios.get(`/size_reviews/?product_id=${targetId}`);
@@ -27,11 +53,14 @@ export default function Product() {
   useEffect(() => {
     if (!id) return;
 
-    getProduct(id);
     getSizeReviews(id);
   }, [id]);
 
-  if (!product) return null;
+  if (!product) return (
+    <div className={styles.loading}>
+      <Spinner />
+    </div>
+  );
 
   return (
     <>
