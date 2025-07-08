3a821f254190513b3186a9f71ec5155d4ca73867
yeeZinu <cube1008@naver.com>
2025-07-08 15:08:25 +0900

    원시 자료형

---
+++ onebite-react/section01/chapter05.js
@@ -0,0 +1,28 @@
+// 원시 타입입
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type
+let myName = "이진우";
+let myLocation = "안양";
+let introduce = myName + myLocation;
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefined Type
+let none;
+console.log(none);
