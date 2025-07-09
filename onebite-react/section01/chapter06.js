d442fbd0405f722263c97b34532bc34b0c478260
yeeZinu <cube1008@naver.com>
2025-07-09 16:09:56 +0900

    형 변환

---
+++ onebite-react/section01/chapter06.js
@@ -0,0 +1,27 @@
+// 1. 묵시적 형 변환
+// -> 자바스크립트 엔진이 알아서 형 변환 하는것
+
+let num = 10;
+let str = "20";
+
+const result = num + str;
+
+// 2. 명시적 형 변환
+// -> 프로그래머 내장함수 등을 이용해서 직접 형 변환을 명시
+// -> 문자열 -> 숫자
+let str1 = "10";
+let strToNum1 = Number(str1);
+
+let str2 = "10개";
+let strToNum2 = parseInt(str2);
+/*
+Number() 와 parsInt()의 차이점
+Number()는 number가 아닌 형식이 섞여있으면 NaN 리턴
+parsInt()는 해당 문자안에 있는 number만 추출해서 변환
+*/
+
+// -> 숫자 -> 문자열
+let num1 = 20;
+let numToStr1 = String(num1);
+
+console.log(numToStr1 + "입니다");
