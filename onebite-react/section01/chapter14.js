14b15d4bb43e84ab763c3e0aeb9e66b4b73148f1
yeeZinu <cube1008@naver.com>
2025-07-10 12:56:26 +0900

    스코프

---
+++ onebite-react/section01/chapter14.js
@@ -0,0 +1,27 @@
+// 스코프
+// -> 전역(전체 영역) 스코프 / 지역(특정 영역) 스코프
+// -> 전역 스코프 : 전체 영역에서 접근 가능
+// -> 지역 스코프 : 특정 영역에서만 접근 가능
+
+let a = 1; // 전역 스코프
+
+function funcA() {
+  let b = 2; // 지역 스코프
+  console.log(a);
+}
+
+funcA();
+
+if (true) {
+  let c = 1;
+}
+
+for (let i = 0; i < 10; i++) {
+  let d = 1;
+  // 함수 선언식은 함수에서만 지역 스코프를 가짐
+  //   function funcB() {
+  //     console.log("hi");
+  //   }
+}
+
+funcB();
